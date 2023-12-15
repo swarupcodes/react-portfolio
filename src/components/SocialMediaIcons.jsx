20232e7d5e7495f72d0c8b45ce46744282059de9
swarupcodes <shakyaswarup31@gmail.com>
2023-12-15 21:56:28 +0545

    first commit

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,41 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.google.com"
+        target="_blank"
+        rel="noreferror"
+      >
+        <img src="../assets/linkedin.png" alt="linkedin-link" />
+      </a>
+
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferror"
+      >
+        <img src="../assets/twitter.png" alt="twitter-link" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferror"
+      >
+        <img src="../assets/facebook.png" alt="facebook-link" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferror"
+      >
+        <img src="../assets/instagram.png" alt="instagram-link" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
